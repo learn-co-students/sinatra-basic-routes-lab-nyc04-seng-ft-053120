3cafc299cafa17292c19c6220cd2675775c93729
gracetian2019 <gracetian4ever@gmail.com>
2020-07-13 09:00:36 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        satus = 200
+        "My name is name"
+    end
+    get '/hometown' do
+        satus = 200
+        "My hometown is hometown"
+    end
+    get '/favorite-song' do
+        satus = 200
+        "My favorite song is favorite-song"
+    end
 end
