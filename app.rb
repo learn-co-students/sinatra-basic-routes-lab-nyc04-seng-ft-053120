2c9503b5f62e974a187787c0639c4ad3f8d4df88
tiggy31 <tigist_alemu@fitnyc.edu>
2020-07-13 08:05:39 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do 
+        "My name is Tigist"
+       end
+       get '/hometown' do 
+        "My hometown is Old Airport"
+       end
+
+       get '/favorite-song' do 
+        "My favorite song is love"
+       end
+
 end
