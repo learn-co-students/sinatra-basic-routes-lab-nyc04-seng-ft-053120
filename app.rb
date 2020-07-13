f94e8d9232f9989ad84110e0d3a7e57ffbbb3661
BarbaraBontempo <barbarabontempo94@gmail.com>
2020-07-13 12:53:58 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+      end
+   
+    get '/name' do 
+        "My name is Barbara"
+    end
+
+    get '/hometown' do 
+        "My hometown is Astoria"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Watermelon Sugar for the time being."
+    end
+
+
 end
