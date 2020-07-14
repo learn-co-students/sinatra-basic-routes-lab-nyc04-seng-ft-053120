3b5526c779dd9157b870cb952379e4a9c107674a
Fraciscv <Francisco.Andaur@icloud.com>
2020-07-14 12:00:10 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Francisco Andaur"
+      end
+    
+    get '/hometown' do
+        "My hometown is Viña Del Mar"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Like Spinning Plates, live version."
+    end
 end
