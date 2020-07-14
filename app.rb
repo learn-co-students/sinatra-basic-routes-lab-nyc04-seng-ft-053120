3e8a70bbe941d2fcaf36ec661a8957b90a93cc3a
tricia3dros <tricia.dros@gmail.com>
2020-07-14 15:18:58 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Tricia"
+    end
+
+    get '/hometown' do
+        "My hometown is Rockaway Beach"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Dreaming"
+    end
+
 end
